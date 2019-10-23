71227fba77cf55729d92a9b234c42cb711924f89
Martin Matějek <martin.matejek@nic.cz>
2019-10-24 00:57:41 +0200

    branch: fix typo

--- svupdater/branch.py
+++ svupdater/branch.py
@@ -5,7 +5,7 @@ def get_os_branch_or_version():
     """Get OS branch or version from uci."""
     with EUci() as uci:
         mode = uci.get("updater", "turris", "mode", dtype=str)
-        value = uci.get("update", "turris", mode, dtype=str, default="")
+        value = uci.get("updater", "turris", mode, dtype=str, default="")
 
     if mode == "version" and not value:
         # fallback to branch when no version is provided
